665910af0ac2d62b078d6198b30cc74c4110d274
AlexSpeleers <alexsieger@gmail.com>
2019-08-22 16:43:19 +0300

    Second commit
    added functionality of swiping gems

--- Match3/Assets/Scripts/Board.cs
+++ Match3/Assets/Scripts/Board.cs
@@ -6,8 +6,10 @@ public class Board : MonoBehaviour
 {
     public int width;
     public int height;
+    public GameObject[] dots;
     public GameObject tilePrefab;
     private BackGroundTile[,] allTiles;
+    public GameObject[,] alldots;
 
     private IEnumerator coroutine;
 
@@ -15,6 +17,7 @@ public class Board : MonoBehaviour
     void Awake()
     {
         allTiles = new BackGroundTile[width, height];
+        alldots = new GameObject[width, height];
     }
 
     private void Start()
@@ -29,10 +32,16 @@ public class Board : MonoBehaviour
             for (int j = 0; j < height; j++)
             {
                 Vector2 tempPos = new Vector2(i, j);
-                GameObject backGroundTile = Instantiate(tilePrefab, tempPos, Quaternion.identity)as GameObject;//set dots under one GO in hierarchy
+                GameObject backGroundTile = Instantiate(tilePrefab, tempPos, Quaternion.identity)as GameObject;//set sells for the dots under one GO in hierarchy
                 backGroundTile.transform.parent = this.transform;
                 backGroundTile.name = $"({i} {j})";
                 yield return new WaitForSeconds(waitTime);
+                //instatiating dots under cells
+                int dotToUse = Random.Range(0, dots.Length);
+                GameObject dot = Instantiate(dots[dotToUse], tempPos, Quaternion.identity);
+                dot.transform.parent = this.transform;
+                dot.name = $"({i} {j})";
+                alldots[i, j] = dot;
             }
         }
     }
---
+++ Match3/Assets/Scripts/Dot.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dot : MonoBehaviour
+{
+    public int column;
+    public int row;
+    public int targetX;
+    public int targetY;
+    private Board board;
+    private GameObject otherDot;
+    private Vector2 firstTouchPos;
+    private Vector2 finalTouchPos;
+    private Vector2 tempPos;
+    public float swipeAngle = 0f;
+    private void Awake()
+    {
+        board = FindObjectOfType<Board>();
+        targetX = (int)transform.position.x;
+        targetY = (int)transform.position.y;
+        row = targetY;
+        column = targetX;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetX = column;
+        targetY = row;
+        if (Mathf.Abs(targetX - transform.position.x) > 0.1)//move dot to the target
+        {
+            tempPos = new Vector2(targetX, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, tempPos, 0.3f);
+        }
+        else
+        {
+            tempPos = new Vector2(targetX, transform.position.y);
+            transform.position = tempPos;
+            board.alldots[column, row] = this.gameObject;
+        }
+        if (Mathf.Abs(targetY - transform.position.y) > 0.1)//move dot to the target
+        {
+            tempPos = new Vector2(transform.position.x, targetY);
+            transform.position = Vector2.Lerp(transform.position, tempPos, 0.3f);
+        }
+        else
+        {
+            tempPos = new Vector2(transform.position.x, targetY);
+            transform.position = tempPos;
+            board.alldots[column, row] = this.gameObject;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        firstTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseUp()
+    {
+        finalTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateAngle();
+    }
+
+    void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(finalTouchPos.y - firstTouchPos.y, finalTouchPos.x - firstTouchPos.x) * 180 / Mathf.PI;//geometry
+        MovePieces();
+    }
+    void MovePieces()
+    {
+        if (swipeAngle > -45 && swipeAngle <= 45 && column < board.width)//right swipe
+        {
+            otherDot = board.alldots[column + 1, row];
+            otherDot.GetComponent<Dot>().column -= 1;
+            column += 1;
+        }
+        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height)//up swipe
+        {
+            otherDot = board.alldots[column, row + 1];
+            otherDot.GetComponent<Dot>().row -= 1;
+            row += 1;
+        }
+        else if ((swipeAngle > 135 || swipeAngle <= -135) && column > 0)//left swipe
+        {
+            otherDot = board.alldots[column - 1, row];
+            otherDot.GetComponent<Dot>().column += 1;
+            column -= 1;
+        }
+        else if (swipeAngle < -45 && swipeAngle >= -135 && row > 0)//down swipe
+        {
+            otherDot = board.alldots[column, row - 1];
+            otherDot.GetComponent<Dot>().row += 1;
+            row -= 1;
+        }
+    }
+}
